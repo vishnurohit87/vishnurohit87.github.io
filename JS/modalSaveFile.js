660d6519ca41eec2605e6dd2252fd10300e672c6
Andrew Tu <drewtu2@yahoo.com>
2016-07-13 11:00:34 -0400

    Updated projects and project descriptions

--- JS/modalSaveFile.js
+++ JS/modalSaveFile.js
@@ -3,8 +3,8 @@ var modalSaveFile = [
 	 "eagleProject",
 	 "eagleProjectGrottoFinal.jpg", 
 	 "<p>My Eagle project was the culmination of the planning, communication, and leadership  skills developed during my 10 years in the Scouting program.</p>" +
-	 "<p>For my project, I designed and built a picnic area and trailhead for a local township wonder: the Hoffheimer Grotto. During this project, I worked closely with township officials, troop leadership, teachers, local historians and friends  for guidance. The project totaled over 500 people hours including more than 100 hours of planning.</p>" +
-	 "<p>Each constructed piece of the project was thoroughly drawn and dimensioned in AutoDesk inventor. I also worked with Adobe Photoshop to design and create a historical poster detailing the history of the Hoffheimer Grotto.</p>",
+		 "<p>For my project, I designed and built a picnic area and trailhead for a local township wonder: the Hoffheimer Grotto. During this project, I worked closely with township officials, troop leadership, teachers, local historians and friends  for guidance. The project totaled over 500 people hours including more than 100 hours of planning.</p>" +
+		 "<p>Each constructed piece of the project was thoroughly drawn and dimensioned in AutoDesk inventor. I also worked with Adobe Photoshop to design and create a historical poster detailing the history of the Hoffheimer Grotto.</p>",
 	 "See the project",
 	 "#eagleDocsModal"],
    
@@ -12,37 +12,62 @@ var modalSaveFile = [
      "firstRobotics",
      "firstRobotics.jpg",
      "During my senior year of high school, I served as the head programmer of my high school FIRST Robotics team. Working direclty with our team mentor, I wrote code in C++ to run the autonomous and tele-operated functions of our robot." +
-     "<br> We finished off our season by competing in the 2015 FIRST Robotics National Championships in St. Louis.",
+     	"<br> We finished off our season by competing in the 2015 FIRST Robotics National Championships in St. Louis.",
      "Read More",
      "N/A"],
     
     ["Gangluff Soundboard - Spring 2015",
      "gangluffSoundboard",
      "Ganglfuff_Soundboard.jpg",
-     "I wrote this soundboard for my AP Chemistry teacher to use to spice up his lectures. The sounds can be played by clicking on screen buttons or using the keyboard. The board also has a data file generator that makes it easy to update the boards on the sound. This project introduced me to Javascript and expanded on my HTML and CSS skills.",
+     "I wrote this soundboard for my AP Chemistry teacher to use to spice up his lectures. " +
+	     "The sounds can be played by clicking on screen buttons or using the keyboard. " +
+	     "The board also has a data file generator that makes it easy to update the boards on the sound. " +
+	     "This project introduced me to Javascript and expanded on my HTML and CSS skills.",
      "Check it out",
      "N/A"],
      
     ["NUCAR: Side Channel Attacks - Fall 2015",
 	"nucar-sca",
 	"RISE_SCA.JPG",
-	"<p>In this project, I implemented several versions of RSA encryption algorithms employing diffrent optimmizations techniques. This project exposed me to higher level computer science concepts and greatly expanded my understanding of C++. Furthemrore, I was introduced to Git, learned to work with linux terminals, and gained experience in developing code in as a team.</p>" +
-	"<p>The year culminated with a poster presentation of our work on side channel attacks at the 2016 Research, Innovation and Scholarship Expo (RISE) at Northeastern University.",
+	"<p>The Northeastern Univeristy Computer Architecture Resarch Lab (NUCAR) Side Channel Attacks project is concerened with cybersecurity on the mobile platform" +
+		"The goal of our project was to exploit weaknesses in current mobile computing standards and break through RSA encryptions using side channel attacks." +
+		"With this information, we would attempt to develop means to guard against these attacks in the future to protect our private data." +
+		"In this project, I implemented several versions of RSA encryption algorithms employing diffrent optimmizations techniques. " +
+		"This project exposed me to higher level computer science concepts and greatly expanded my understanding of C++. " +
+		"Furthemrore, I was introduced to Git, learned to work with linux terminals, and gained experience in developing code in as a team.</p>" +
+		"<p>The year culminated with a poster presentation of our work on side channel attacks at the 2016 Research, Innovation and Scholarship Expo (RISE) at Northeastern University.",
 	"See our poster!",
 	"Docs/SideChannelPosterFinal.pdf"],
       
     ["NUMONET: Underwater Acoustic Networking - Fall 2015",
      "numonet",
      "NUMONET.pdf",
-     "DESCRIPTION OF NUMONET WORK",
+     "The Northeastern Univeristy Marine Observatory Network (NU MONET) project aims to build an underwater accoustic network at Northeastern's Marine Science center in Nahant, MA." +
+     	"I joined this project in its early stages in October of 2015 after meeting the team at an Undergraduate Research Fair held at Northeastern and it has since become one of my primary focuses." +
+     	"Through the this project, I have been exposed to a wide range of technologies and areas, ranging from Matlab, to microcontrollers and circuitry, to state of the art underwater acoustic modems." +
+     	"I have been fortuante enought to work on two (soon to be) published papers and the unique opportunity to work with a team in Rome, Italy." +
+     	"Finally, I was also given the chance to redesign our project's website where more information about our project can be found. Click the button below to read more!",
      "Read More",
-     "http://nu_monet.coe.neu.edu"],
+     "http://northeastern.edu/numonet"],
      
      
     ["My Personal Website - Winter 2015",
      "personalSite",
      "PersonalSite.jpg",
-     "During my freshman year, I realized the importance of a portfolio of sorts to showcase my accomplishments. A research colleague showed me a website he put togther and inspired me to create my own. While I had some past experience with web design, the new site is considerably more advanced than anything I had previously worked on. In order to complete this project, I learned to use Boostrap and jQuery in order to create a simple but attractive portfolio.",
+     "During my freshman year, I realized the importance of a portfolio of sorts to showcase my accomplishments. " +
+     	"A research colleague showed me a website he put togther and inspired me to create my own. " +
+     	"While I had some previous experience with web design, the new site is considerably more advanced than anything I had previously worked on. " +
+     	"In order to complete this project, I learned to use Boostrap and jQuery in order to create a simple but attractive portfolio.",
      "Visit Site",
-     "http://drewtu2.github.io"]
+     "http://drewtu2.github.io"],
+    
+    ["Northeatern Interactive Clustering Engine (NICE) - Summer 2015",
+     "NICE",
+     "NICE.jpg",
+     "Over the summer, I decided I wanted to gain technical experince in two aspects: fundamental development practices and an introduction to machine learning." +
+     	"In order to persue such a task, I joined another research project: the Northeatern University Interactive Clustering Engine, more colloquially known as the \"NICE\" project." +
+     	"<p> The goal of the NICE project is to develop a web tool that researchers can use to better understand their data by using machine learning and data visualizaiton algorithms. " +
+     	"<p> In this project, I learned aobut software engineering, developed a better understanding of Git, employed specific coding styles, and worked with Google Tests.",
+     "See presentation",
+     "N/A"],
 ];
