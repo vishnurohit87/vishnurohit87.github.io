d39d47113b10337d2241b9e10bd6bcd5d481d054
jagatpreet <nir.j@northeastern.edu>
2023-11-05 18:02:00 -0500

    updated project descriptions

--- JS/modalSaveFile.js
+++ JS/modalSaveFile.js
@@ -14,24 +14,25 @@ var modalSaveFile = [
       "https://github.com/jpsnir/portfolio_projects/"],
     
     ["Humatics | Algorithms Engineer Intern", 
-     "humatics_coop",
+     "humatics_intern",
      "humatics_logo_intern.png",
      "Sensor fusion and software integration with Humatics Rail positioning technology; designed digital filters to filter the noise in IMU data; IMU self calibration routines, real time processing, design patterns, Train attitude estimation algorithms for train's acceleration correction,  ",
      "Read more!",
      "N/A"],
-    
-    ["Teaching assistant 
-     "teaching_assistant",
-     "teaching_assistant_logo.jpg",
-     "Write up for teaching assistant",
-     "Check it out",
-     "N/A"],
+
+    ["Leadership in mentorship and teaching",
+     "teaching_assitant",
+     "teaching_assistant_logo.png",
+     "Track record for leading and performing teaching assistant positions in IIT Delhi, Northeastern university and young technology scholars program", 
+     "Check out the details here",
+     "http://github.com/jpsnir/portfolio_projects/"],
     
     ["Control Design engineer",
      "control_design_engineer",
      "excavator_simulator.webp",
+     "Systems and control design engineer at Faros Simulation systems, leading to success of various projects that include motion platforms, simulation software, and motion control interface electronics for seamless integration. 
      "Check it out",
-     "http://drewtu2.github.io/gangluff-soundboard/GangluffSoundBoard.html"],
+     "http://github.com/jpsnir/portfolio_projects/"],
 ];
 
 
