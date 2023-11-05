38f91ad39a4b6d339dfa6585c35dc78e7178537d
jagatpreet <nir.j@northeastern.edu>
2023-11-05 16:03:56 -0500

    updated content of projects

--- JS/modalSaveFile.js
+++ JS/modalSaveFile.js
@@ -2,12 +2,7 @@ var modalSaveFile = [
       ["Covariance recovery from a deep learning VSLAM solution using principles in non linear optimization",
       "covariance_recovery",
       "covariance_recovery.png",
-      "To fuse the VO solution from pretrained VO engine, we need to get the scaled covariance from DROID SLAM engine."+
-      "In a general non-linear regression problem shown on the right, information of the optimal solution can be recovered from the knowledge of the jacobians at optimal point and the noise. 
-      "Inverse of the information matrix is the covariance." + 
-      " After we recover the information of the obtained solution, we need to introduce gauge freedom for maintaining consistency in the covariances." + 
-      "The VO solution is ill defined in scale so the covariance should be defined only for 5 degrees of freedom." +
-       "Once we obtain the information matrix of the VO solution, we can fuse it with the IMU measurements" ,
+      "To fuse the VO solution from pretrained VO engine, we need to get the scaled covariance from DROID SLAM engine. In a general non-linear regression problem shown on the right, information of the optimal solution can be recovered from the knowledge of the jacobians at optimal point and the noise. Inverse of the information matrix is the covariance. After we recover the information of the obtained solution, we need to introduce gauge freedom for maintaining consistency in the covariances. The VO solution is ill defined in scale so the covariance should be defined only for 5 degrees of freedom. Once we obtain the information matrix of the VO solution, we can fuse it with the IMU measurements" ,
       "Github will be released soon!",
       "https://github.com/jpsnir/portfolio_projects"],
       
