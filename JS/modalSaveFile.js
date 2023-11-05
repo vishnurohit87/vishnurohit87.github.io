5e8ed759900751b43c5946d8b0decdaf1bad1755
jagatpreet <nir.j@northeastern.edu>
2023-11-05 18:30:20 -0500

    updated projects

--- JS/modalSaveFile.js
+++ JS/modalSaveFile.js
@@ -30,9 +30,51 @@ var modalSaveFile = [
     ["Control Design engineer",
      "control_design_engineer",
      "excavator_simulator.webp",
-     "Systems and control design engineer at Faros Simulation systems, leading to success of various projects that include motion platforms, simulation software, and motion control interface electronics for seamless integration. 
+     "Systems and control design engineer at Faros Simulation systems, leading to success of various projects that include motion platforms, simulation software, and motion control interface electronics for seamless integration.", 
      "Check it out",
      "http://github.com/jpsnir/portfolio_projects/"],
+    
+    ["Controller for assistive Writing device for Parkinson’s patient",
+     "assistive_writing",
+     "assistive_robotics.png",
+     " Write up for the project",
+     "Check out the report",
+     "http://github.com/jpsnir/portfolio_projects/"],
+      
+      ["Modeling and Simulation of Three link Biped using Hybrid Zero Dynamics",
+      "three_link_biped",
+      "three_link_biped.png",
+      "The control of a bipedal system is a challenging and complex problem that requires a powerful solution. The method of Hybrid Zero Dynamics developed at the University of Michigan is one such solution. This paper details the complete implementation of the hybrid zero dynamics of a three-link biped through the development of the kinematics, dynamic modeling, and controller design. Using the zero dynamics of our derived model, we find an energy efficient gait design, and integrate the full dynamics with a PD controller. These results are then verified with a step simulation.",
+      "Read the writeup!",
+      "http://github.com/jpsnir/portfolio_projects/"],
+
+    ["Data association techniques for image matching - classical and deep learning based",
+     "2d_visual_odom",
+     "2d_visual_odom.png",
+     "Developed a basic underwater visual odometry pipleine using OpenCV on a real dataset from an underwater autonomous vehicle. Estimated and caclulated potential loop closures. Optimized proposed solutions using g2o.",
+     "See the Github!",
+     "https://github.com/jpsnir/portfolio_projects/"],
+
+    ["Visual Odometry for 2D and 3D navigation with real datasets with loop closure",
+     "3d_visual_odom",
+     "3d_visual_odom.png",
+     "Kitti, underwater dataset",
+     "See the Github!",
+     "https://github.com/jpsnir/portfolio_projects/"],
+    
+    ["Depth reconstruction and robust VSLAM in dynamic environments",
+     "depth_reconstruction",
+     "robust_vslam_dynamic_environments.png",
+     "Developed a basic 3d monocular visual odometry pipeline using OpenCV and attempted to run it on the Kitti dataset. Was only able to keep track for a couple of frames before losing track of features. None the less, this project proved to be a very interesting starting point into visual SLAM.",
+     "See the paper!",
+     "https://ieeexplore.ieee.org/document/9591171"],
+
+    ["System design and Sensor Calibration Techniques - Camera, Lidar and IMU",
+     "sensor_calibration",
+     "systems_and_calibration.png",
+     "Description here",
+     "See more details!",
+     "https://github.com/jpsnir/portfolio_projects/"],
 ];
 
 
